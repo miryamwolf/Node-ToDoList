a4a95b327c1bc12901dd6cf59c9fe2f1093e9df6
m0556722858 <m0556722858@gmail.com>
2025-03-18 13:37:38 +0200

    aaa

---
+++ app.js
@@ -0,0 +1,67 @@
+<<<<<<< HEAD
+import express from 'express'
+import axios from 'axios';
+import dotenv from'dotenv'
+const app = express();
+const port = 3000;
+
+dotenv.config();
+// Middleware to parse JSON
+app.use(express.json());
+const autho=process.env.RENDER_API_KEY;
+
+// Endpoint to get installed apps
+app.get('/', async (req, res) => {
+    try {
+        const response = await axios.get('https://api.render.com/v1/services?includePreviews=true&limit=20', {
+            headers: {
+                'Authorization': `Bearer rnd_Me0Cl11eIIuVEXK3AYREjEtFpTyg`,
+                'Accept': 'application/json'
+                
+            }
+        });
+        res.json(response.data);
+    } catch (error) {
+        console.log(process.env.RENDER_API_KEY);
+        console.error(error);
+        res.status(500).json({ error: 'Failed to fetch apps' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+=======
+import express from 'express'
+import axios from 'axios';
+import dotenv from'dotenv'
+const app = express();
+const port = 3000;
+
+dotenv.config();
+// Middleware to parse JSON
+app.use(express.json());
+const autho=process.env.RENDER_API_KEY;
+
+// Endpoint to get installed apps
+app.get('/', async (req, res) => {
+    try {
+        const response = await axios.get('https://api.render.com/v1/services?includePreviews=true&limit=20', {
+            headers: {
+                'Authorization': `Bearer rnd_Me0Cl11eIIuVEXK3AYREjEtFpTyg`,
+                'Accept': 'application/json'
+                
+            }
+        });
+        res.json(response.data);
+    } catch (error) {
+        console.log(process.env.RENDER_API_KEY);
+        console.error(error);
+        res.status(500).json({ error: 'Failed to fetch apps' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+>>>>>>> ad08b64ff3c6666723829e5502ef96b89a0b6366
